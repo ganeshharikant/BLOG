f12dd2e1e1d0a3833d28ce7c89bcbaf50ca95329
Ganesh <ganeshh9876@gmail.com>
2024-08-26 14:26:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const { v4: uuidv4 } = require('uuid');
+var methodOverride = require('method-override')
+
+
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(methodOverride('_method'));
+let posts = [
+
+    {  id:uuidv4(),
+        username: "ganesh",
+        content: "i have secured rank"
+
+    },
+    {     id:uuidv4(),
+        username: "adasrh",
+        content: "i have second rank rank"
+
+    },
+
+];
+
+app.get("/posts", (req, res) => {
+     res.render("index.ejs",{posts});
+
+
+
+});
+app.get("/posts/new", (req,res)=> {
+   res.render("new.ejs");
+
+});
+app.post("/posts",(req,res) =>{
+let id =uuidv4();
+let {username,content}=req.body;
+posts.push({id,username,content});
+res.redirect("/posts")
+
+});
+app.get("/posts/:id",(req,res)=>{
+  let  {id} =req.params;
+  let post=posts.find((p) => id===p.id);
+  console.log(post);
+  res.render("show.ejs",{post});
+
+
+
+});
+app.get("/posts/:id/edit", (req, res) => {
+    let  { id } = req.params;
+ let post = posts.find((p) =>id===p.id);
+ res.render("edit.ejs", { post });
+});
+
+
+app.patch("/posts/:id",(req,res) => {
+    let { id } =req.params;
+    console.log(req.body);
+    let newContent=req.body.content;
+    let post =posts.find((p)=>id===p.id);
+    post.content=newContent;
+    res.redirect("/posts");
+
+
+
+});
+app.delete("/posts/:id",(req,res) => {
+
+let{ id }= req.params;
+posts=posts.filter((p) => id !==p.id);
+res.redirect("/posts");
+
+});
+
+app.listen(port, () => {
+
+    console.log("okayy");
+
+});
